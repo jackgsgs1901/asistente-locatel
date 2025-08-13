30d6a9eb94f75f6e5d92513b6ba5f4751bb9bf31
jackgsgs18 <gomovilservidor@gmail.com>
2025-08-13 01:03:25 -0400

    Habilitar CORS para permitir frontend

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import fetch from 'node-fetch';
+
 
 dotenv.config();
 
@@ -11,6 +13,7 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(cors());
 
 app.use(express.json());
 app.use(express.static(__dirname)); // para servir index.html y assets
